eefcf1aed2d69b710a0a11d3fd0f4f21017334b9
loyal-recipe-0842 <riveraronirae@gmail.com>
2019-06-10 01:57:46 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
-def display_rainbow(colors)
+def display_rainbow(colors['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
   puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
   puts "The first color of the rainbow is: #{colors[1]}"
   puts "The second color of the rainbow is: #{colors[2]}"
