443ff65993b5db389a480e92e3b11a37f05f94b1
loyal-recipe-0842 <riveraronirae@gmail.com>
2019-06-10 01:23:36 +0000

    Automatically backed up by Learn

--- print_business_card.rb
+++ print_business_card.rb
@@ -5,10 +5,3 @@ end
 
 bob     = ["Bob", 46, "Juggler", "555-555-5555"]
 stefani = ["Stefani", 49, "Firefighter", "555-111-1111"]
-
-print_business_card(bob)
-
-# => "Name: Bob, Age: 46, Occupation: Juggler"
-# => "Contact: 555-555-5555"
-
-print_business_card(stefani)
